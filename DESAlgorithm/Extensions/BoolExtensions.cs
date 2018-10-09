34420e1f4a43c97e34cdf4a28bb7e666e8b79bad
jarkuszynski <37298865+jarkuszynski@users.noreply.github.com>
2018-10-09 09:52:55 +0200

    S-Block Algorithm

---
+++ DESAlgorithm/Extensions/BoolExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DESAlgorithm.Extensions
+{
+    public static class BoolExtensions
+    {
+        public static int ToInt(this bool value)
+        {
+            return value == true ? 1 : 0;
+        }
+    }
+}
