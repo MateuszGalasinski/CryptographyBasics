7348345bf190a1401b43f19c390b11ec3384b798
Mateusz Galasinski <Mateusz.Galasinski@vistex.com>
2018-10-12 13:35:08 +0200

    Decrypt steps ( not tested yet)

--- DESAlgorithm/CryptoAlgorithm.cs
+++ DESAlgorithm/CryptoAlgorithm.cs
@@ -24,27 +24,34 @@ namespace DES
 
         public bool[] Decrypt(bool[] data)
         {
-            //foreach (IDataTransformation transformation in _decryptSteps)
-            //{
-            //    bitData = transformation.Transform(bitData);
-            //}
+            bool[] unpaddedData = _paddingStrategy.RemovePadding(data);
 
-            return data;
+            return TrasformateWithBlockSeparation(_encryptSteps, unpaddedData);
         }
 
         public bool[] Encrypt(bool[] data)
         {
-            List<DataSet> encryptedBlocks = new List<DataSet>();
-
             bool[] paddedData = _paddingStrategy.AddPadding(data);
 
-            for (int i = 0; i < paddedData.Length / 64; i++)
+            return TrasformateWithBlockSeparation(_encryptSteps, paddedData);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="transformations"></param>
+        /// <param name="data">Padded data</param>
+        /// <returns></returns>
+        private bool[] TrasformateWithBlockSeparation(List<IDataTransformation> transformations, bool[] data)
+        {
+            List<DataSet> encryptedBlocks = new List<DataSet>();
+            for (int i = 0; i < data.Length / 64; i++)
             {
                 bool[] leftHalfBlock = new bool[32];
-                Array.Copy(paddedData, i * 64, leftHalfBlock, 0, 32);
+                Array.Copy(data, i * 64, leftHalfBlock, 0, 32);
                 bool[] rightHalfBlock = new bool[32];
-                Array.Copy(paddedData, i * 64 + 32, leftHalfBlock, 0, 32);
-                
+                Array.Copy(data, i * 64 + 32, leftHalfBlock, 0, 32);
+
                 DataSet dataSet = new DataSet()
                 {
                     Left = new BitArray(leftHalfBlock),
@@ -59,7 +66,7 @@ namespace DES
                 encryptedBlocks.Add(dataSet);
             }
 
-            bool[] encryptedData = new bool[encryptedBlocks.Count*64];
+            bool[] encryptedData = new bool[encryptedBlocks.Count * 64];
             int blocksCounter = 0;
             foreach (DataSet encryptedBlock in encryptedBlocks)
             {
