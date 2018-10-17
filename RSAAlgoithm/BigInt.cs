5f0ec5d08a108476fd7015cbd86a15c9c0900fd3
210241 <210241@edu.p.lodz.pl>
2018-10-17 17:48:50 +0200

    Init branch commit

---
+++ RSAAlgoithm/BigInt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigIntTest
+{
+    class BigInt
+    {
+
+    }
+}
