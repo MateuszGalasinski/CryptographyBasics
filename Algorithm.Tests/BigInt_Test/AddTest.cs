40cf8b2438b9cc291400e52f80d827d68adbde5d
210241 <210241@edu.p.lodz.pl>
2018-10-18 21:07:26 +0200

    Addition tested should work, problem with Subtract

--- Algorithm.Tests/BigInt_Test/AddTest.cs
+++ Algorithm.Tests/BigInt_Test/AddTest.cs
@@ -11,14 +11,80 @@ namespace Algorithm.Tests.BigInt_Test
     class AddTest
     {
         [Test]
-        public void ThreeNines_Test()
+        public void SameLength_ResultShouldBeOneDigitLonger_WithCarry()
         {
             int[] first = {9, 9, 9 };
             int[] second = {9,9,9};
             
             int[] result = BigInt.Add(first, second);
 
-            Assert.AreEqual(new int[]{1,9,9}, result);
+            Assert.AreEqual(new int[]{8,9,9,1}, result);
+        }
+
+        [Test]
+        public void SameLength_NoCarry()
+        {
+            int[] first = { 8, 2, 1 };
+            int[] second = { 1, 2, 3 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 9,4,4 }, result);
+        }
+
+        [Test]
+        public void FirstLonger_WithCarry()
+        {
+            int[] first = {9, 8, 2, 1 };
+            int[] second = { 5, 2, 3 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 4, 1, 6,1 }, result);
+        }
+
+        [Test]
+        public void FirstLonger_NoCarry()
+        {
+            int[] first = { 7, 8, 2, 1 };
+            int[] second = { 1, 1, 1 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 8, 9, 3, 1 }, result);
+        }
+
+        [Test]
+        public void SecondLonger_WithCarry()
+        {
+            int[] second = { 9, 8, 2, 1 };
+            int[] first = { 5, 2, 3 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 4, 1, 6, 1 }, result);
+        }
+
+        [Test]
+        public void SecondLonger_NoCarry()
+        {
+            int[] second = { 7, 8, 2, 1 };
+            int[] first = { 1, 1, 1 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 8, 9, 3, 1 }, result);
+        }
+
+        [Test]
+        public void SecondspamLonger_NoCarry()
+        {
+            int[] second = { 0,0,3,6,9 };
+            int[] first = { 1,4,7,6 };
+
+            int[] result = BigInt.Add(first, second);
+
+            Assert.AreEqual(new int[] { 1,4,0,3,0,1 }, result);
         }
     }
 }
--- Algorithm.Tests/BigInt_Test/ModTest.cs
+++ Algorithm.Tests/BigInt_Test/ModTest.cs
@@ -6,14 +6,60 @@ namespace Algorithm.Tests.BigInt_Test
     class ModTest
     {
         [Test]
-        public void ThreeNines_Test()
+        public void ShouldBeNightyNine_Test()
         {
             int[] first = { 9, 9, 9, 9, 9 };
             int[] second = { 9, 9, 9 };
 
             int[] result = BigInt.Mod(first, second);
 
-            Assert.AreEqual(new int[] { 99 }, result);
+            Assert.AreEqual(new int[] { 9,9 }, result);
         }
+
+        [Test]
+        public void ShouldBeNine_Test()
+        {
+            int[] first = {  9, 9, 9, 9,9,9,9 };
+            int[] second = { 9, 9, 9 };
+
+            int[] result = BigInt.Mod(first, second);
+
+            Assert.AreEqual(new int[] { 9 }, result);
+        }
+
+        [Test]
+        public void SameNumbers_ShouldBeZero()
+        {
+            int[] first = { 9, 9, 9 };
+            int[] second = { 9, 9, 9 };
+
+            int[] result = BigInt.Mod(first, second);
+
+            Assert.AreEqual(new int[] { 0 }, result);
+        }
+
+        [Test]
+        public void FirstShorter_ShouldEqualFirst()
+        {
+            int[] first = { 9, 9, 9 };
+            int[] second = { 9, 9, 9, 9 };
+
+            int[] result = BigInt.Mod(first, second);
+
+            Assert.AreEqual(new int[] { 9,9,9 }, result);
+        }
+
+        [Test]
+        public void RandomNumbers_Test()
+        {
+            int[] first = {7,6,5,4,3,2,1};
+            int[] second = { 4,3,2,1};
+
+            int[] result = BigInt.Mod(first, second);
+
+            Assert.AreEqual(new int[] { 7,6,5}, result);
+        }
+
+
     }
 }
