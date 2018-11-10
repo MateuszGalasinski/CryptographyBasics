a6ff2b8882524e838bbaa488f9684678d1793ca1
Mateusz Galasinski <Mateusz.Galasinski@vistex.com>
2018-11-10 17:27:54 +0100

    Padding with some tests

---
+++ RSA/Padding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RSA
+{
+    public static class Padding
+    {
+        public static byte[] AddPadding(byte[] data, int keyLength)
+        {
+            byte[] result;
+
+            if (data.Length % keyLength == 0)
+            {
+                result = new byte[data.Length];
+                data.CopyTo(result, 0);
+            }
+            else
+            {
+                int numberOfBlocksInSource = data.Length / keyLength;
+                int additionalLength = ((numberOfBlocksInSource + 1) * keyLength) - data.Length;
+                result = new byte[data.Length + additionalLength];
+                data.CopyTo(result, 0);
+
+                for (int i = 0; i < additionalLength; i++)
+                {
+                    result[numberOfBlocksInSource * keyLength + i] = 0;
+                }
+
+                Array.Copy(data,
+                    numberOfBlocksInSource * keyLength,
+                    result,
+                    numberOfBlocksInSource * keyLength + additionalLength,
+                    keyLength - additionalLength);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ RSATests/PaddingTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RSA;
+
+namespace RSATests
+{
+    public class PaddingTests
+    {
+        [TestCase(2,
+            new byte[] { 0x0A },
+            new byte[] { 0x0, 0x0A })]
+        [TestCase(4,
+            new byte[] { 0x0A },
+            new byte[] { 0x0, 0x0, 0x0, 0x0A })]
+        [TestCase(4,
+            new byte[] { 0x0, 0x0A },
+            new byte[] { 0x0, 0x0, 0x0, 0x0A })]
+        [TestCase(4,
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 },
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 })]
+        [TestCase(2,
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 },
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 })]
+        [TestCase(2,
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 },
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 })]
+        [TestCase(3,
+            new byte[] { 0x0, 0xD, 0x0A, 0x0 },
+            new byte[] { 0x0, 0xD, 0x0A, 0x0, 0x0, 0x0 })]
+        [TestCase(3,
+            new byte[] { 0x0, 0xD, 0x0A, 0xC },
+            new byte[] { 0x0, 0xD, 0x0A, 0x0, 0x0, 0xC })]
+        public void PaddingTest(int keyLength, byte[] data, byte[] correctlyPadded)
+        {
+            byte[] padded = Padding.AddPadding(data, keyLength);
+
+            padded.Should().BeEquivalentTo(correctlyPadded);
+        }
+    }
+}
