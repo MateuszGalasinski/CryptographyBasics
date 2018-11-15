139e5dcaa2a6cfa992dd8419f3e34a11de1dec80
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-11-15 21:32:52 +0100

    generator with simple test

---
+++ SchnorDigitalSign/GeneratorGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace SchnorDigitalSign
+{
+    public static class GeneratorGenerator
+    {
+        public static BigInteger Generate(BigInteger p, BigInteger q)
+        {
+            BigInteger a = new BigInteger();
+            for (BigInteger h = 1; h < q; h++)
+            {
+                BigInteger exponent = (p - 1) / q;
+                a = BigInteger.ModPow(h, exponent, p);
+                if (a != 1)
+                {
+                    break;
+                }
+            }
+
+            return a;
+        }
+    }
+}
--- SchnorDigitalSign/SchnorrAlgorithm.cs
+++ SchnorDigitalSign/SchnorrAlgorithm.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using SchnorDigitalSign.Model;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using SchnorDigitalSign.Model;
 
 namespace SchnorDigitalSign
 {
