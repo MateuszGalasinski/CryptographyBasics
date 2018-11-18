6e6215ed4dc0d2f43bdb73cdb0023dd6843a3b97
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-11-18 22:34:20 +0100

    some padding test

--- RSATests/PaddingTests.cs
+++ RSATests/PaddingTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using RSA;
 using RSA.PaddingStrategies;
 
 namespace RSATests
@@ -29,6 +30,31 @@ namespace RSATests
             byte[] paddedData = _paddingStrategy.AddPadding(dataToPadd, blockLength);
 
             paddedData.Should().BeEquivalentTo(correctData);
+        }
+
+        [Test]
+        public void AddBigIntegerPaddingTest()
+        {
+            int blockSize = 4;
+            var chunker = new DataChunker();
+            var data = new BigInteger[]
+            {
+                new BigInteger(new byte[]
+                {
+                    0x03, 0x02, 0x01,
+                }),
+                new BigInteger(new byte[]
+                {
+                    0x03, 0x02, 0x01,
+                })
+            };
+
+            var padded = chunker.MergeDataAndRemovePadding(data, blockSize);
+
+            padded.ShouldBeEquivalentTo(new byte[]
+            {
+                0x0, 0x03, 0x02, 0x01, 0x0, 0x03, 0x02
+            });
         }
     }
 }
